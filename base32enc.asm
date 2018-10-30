457af5ece46ff48489a80dc7d6642cb5508beecb
Marc Muster <marc.muster@bluewin.ch>
2018-10-30 20:10:32 +0100

    ugly af but encoding works

--- base32enc.asm
+++ base32enc.asm
@@ -1,13 +1,14 @@
 SECTION .data			; Section containing initialised data
 	BASE32_TABLE: db "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567"
 	base32 equ 5 		; group into 5 bits
+	equalSign: db "="
 	
 SECTION .bss			; Section containing uninitialized data
-	input:	resb 16
-	inputLen equ 16
+	input:	resb 256
+	inputLen equ 256
 	bitsNext resb 1		; the amount of bits we need from the next byte
 	offset resb 1		; offset from where we start counting 5 bits
-	output:	resb 16		; output of string encoded in base32
+	output:	resb 1		; output of string encoded in base32
 	
 SECTION .text			; Section containing code
 	global 	_start		; Linker needs this to find the entry point!
@@ -22,16 +23,18 @@ Read:
 	mov rsi, input		; Pass offset of the input
 	mov rdx, inputLen	; Pass number of bytes to read
 	syscall
+
+	cmp eax, 0
+	je _done
 	
 	;; Set up the registers for the process buffer step
-	xor ecx, ecx		; Clear line string pointer to 0
+	xor ecx, ecx		; Clear line string pointer to 0y
 	
 	mov dil, 0		; sil holds the current turn number
 	mov r9b, 0		; r9b holds the offset
-	mov r10, rax		; r10b holds the amount of characters from the input + 1
-	dec r10			; remove the extra byte amount
-	mov r11b, 0		; r11b holds the amount of bytes we have already processed
+	mov r10, rax		; r10 holds the amount of characters from the input + 1
 	mov bl, 0		; bl holds the return of f(turn)
+	mov r8, 0		; amount of output characters
 	
 Scan:
 	xor eax, eax		; Clear eax to 0
@@ -50,8 +53,8 @@ nextTurn:
 	mov al, [rsi+rcx]	; put the char + the offset of the input in al
 
 	cmp r9b, 3		; compare the offset to 3
+	je currentAndIncrement 	; if == 3
 	jle fromCurrent		; if < 3 
-	je currentAndIncrement	; if = 3
 	Jg fromCurrentAndNext	; if > 3
 
 	;; we have enough bits and dont need any from the next byte
@@ -75,9 +78,16 @@ fromCurrent:
 	
 
 currentAndIncrement:
-	shr al, 3
-	;; PRINT AL
+	and al, 1Fh		; get the last 5 bits
+	mov r9, 0		; set the offset back to 0
+	push rcx
+	call lookupAndSaveToOutput
+	pop rcx
+
 	inc rcx
+	cmp rcx, r10		; test end condition
+	je _printEqualSigns	;
+	
 	jmp nextTurn
 
 	;; we need bits from the current one AND from the next one
@@ -88,19 +98,19 @@ fromCurrentAndNext:
 	;; next bits from the next byte
 	mov cl, r9b		; move the offset to cl
 	shl al, cl		; shift left by the offset to null out the left bits
-
-	;; al should be 8
-	mov cl, 8		; mov 8 to cl
-	sub cl, r9b		; - offset
-	shr al, 3		; shift back right by 8 - offset
-	
-	;; al should be 4
+	shr al, 3		; shift back right by 3
 	
 	pop cx			; get cx from the stack
 
+	cmp rcx, r10
+	je _lookupAndPrintEqualSigns	
+	
 	;; inrement and get the first bits from the next byte
 	inc rcx			; next byte
-	mov dl, [rsi+rcx]	; mov the next byte to al
+	
+	;; check the end condition here already?
+	
+	mov dl, [rsi+rcx]	; mov the next byte to dl
 
 	push cx			; save cx to the stack
 	
@@ -117,17 +127,15 @@ fromCurrentAndNext:
 	call lookupAndSaveToOutput
 	pop cx			; restore cx
 	
-T:
 	push cx			; save
-	cmp rcx, r10
+	cmp rcx, r10		; compare the input counter to the initial input length
 	pop cx			; restore
-	je _done
+	je _printEqualSigns	; if the input counter is equal to the initial length we're done
+	;; aka we gotta print the equal signs first
 	
 	mov r9b, bl		; move f(turn) to the offset
 	
 	jmp nextTurn
-	
-	call _done
 	nop
 
 lookupAndSaveToOutput:
@@ -144,7 +152,7 @@ _printOutput:
 	mov rax, 1		; Code for Sys_write call
 	mov rdi, 1		; Standard Output
 	mov rsi, output		; Adress of the output
-	mov rdx, 16		; size of the input
+	mov rdx, 1		; size of the output
 	syscall			; Make kernel call
 
 	pop rdx
@@ -152,6 +160,51 @@ _printOutput:
 	pop rdi
 	pop rax
 	pop rcx
+	inc r8
+	cmp r8, 9	     ; if we already put out 8 characters reset it to 1
+	je resetCountTo1
+	jne donePrinting
+
+resetCountTo1:
+	mov r8, 1
+
+donePrinting:
+	ret
+
+_lookupAndPrintEqualSigns:
+	call lookupAndSaveToOutput
+	jmp _printEqualSigns
+	
+;;; print the correct amount of equal signs
+;;; this is achieved by subtracting 8 from rcx
+_printEqualSigns:
+	mov rax, r8
+	mov rcx, 8
+	sub rcx, rax
+loop:
+	cmp rcx, 0
+	je _done
+
+	push rcx
+	push rax
+	push rdi
+	push rsi
+	push rdx
+	call _printOneEqualSign
+	pop rdx
+	pop rsi
+	pop rdi
+	pop rax
+	pop rcx
+	dec rcx
+	jmp loop
+
+_printOneEqualSign:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, equalSign	; equal sign
+	mov rdx, 1		; output length of 1
+	syscall
 	ret
 	
 _done:
